59a5e08ce30e0d80f49f71ed18f5ded9509b50c6
kaaymaa06 <kaaymaa06@gmail.com>
2026-04-14 11:06:18 +0700

    Update index.blade.php

--- resources/views/anggota/riwayat/index.blade.php
+++ resources/views/anggota/riwayat/index.blade.php
@@ -54,17 +54,28 @@
 
                     {{-- STATUS --}}
                     <td class="px-4 py-3">
+                         @php
+                            $jatuhTempo = \Carbon\Carbon::parse($item->jatuh_tempo);
+                        @endphp
+
                         @if ($item->status == 'selesai')
                             <span class="px-3 py-1 text-xs bg-green-100 text-green-700 rounded-full">
                                 Selesai
                             </span>
+
                         @elseif ($item->status == 'ditolak')
                             <span class="px-3 py-1 text-xs bg-red-100 text-red-700 rounded-full">
                                 Ditolak
                             </span>
+
+                        @elseif (\Carbon\Carbon::now()->gt($jatuhTempo) && $item->status != 'selesai')
+                            <span class="px-3 py-1 text-xs bg-red-100 text-red-700 rounded-full">
+                                Terlambat
+                            </span>
+
                         @else
-                            <span class="px-3 py-1 text-xs bg-gray-200 text-gray-700 rounded-full">
-                                {{ $item->status }}
+                            <span class="px-3 py-1 text-xs bg-green-100 text-green-700 rounded-full">
+                                Dipinjam
                             </span>
                         @endif
                     </td>
